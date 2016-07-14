e5ef42a9f9718c5a3f8da386e91e9e0dc4fef450
Stephen McIntosh <stephenmac7@gmail.com>
2016-07-14 11:30:21 -0400

    Commented unused support method

--- spec/support/yaml_fixture.rb
+++ spec/support/yaml_fixture.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 module YAMLFixture
+=begin
   def from_file(name)
     RadHoc.new(File.open(File.join(Bundler.root, "spec/fixtures/yaml/#{name}.yaml")))
   end
+=end
 
   def from_literal(literal)
     RadHoc.new(literal)
